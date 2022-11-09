79eebae1d7f555e533cef950bf8268b4d056e9d8
unknown <unidiogo@outlook.com>
2022-11-09 17:07:30 -0300

    Criação do componente botão com alguma estilização

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function criaBotão() {
-    return ``
+function criaBotãoNum(n) {
+    return `<button class ="num" onclick="">${n}</button>`
 }
+
+function criaBotãoOp(op) {
+    return `<button class ="op" onclick="">${op}</button>`
+}
+
+
+document.querySelector('#container').innerHTML =
+    criaBotãoNum(1) +
+    criaBotãoNum(2) +
+    criaBotãoOp("+")
