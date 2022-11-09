8cc0c3cf5f2e2fd3254b8b451dfb9818b6cdaa95
unknown <unidiogo@outlook.com>
2022-11-09 20:19:58 -0300

    Implementação de botões númericos

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-function criaBotãoNum(n) {
-    return `<button class ="num" onclick="">${n}</button>`
+function criaBotãoNum(a) {
+    return `<button class ="num" onclick="">${a}</button>` 
 }
+
 
 function criaBotãoOp(op) {
     return `<button class ="op" onclick="">${op}</button>`
@@ -10,4 +11,12 @@ function criaBotãoOp(op) {
 document.querySelector('#container').innerHTML =
     criaBotãoNum(1) +
     criaBotãoNum(2) +
-    criaBotãoOp("+")
+    criaBotãoNum(3) +
+    criaBotãoNum(4) +
+    criaBotãoNum(5) +
+    criaBotãoNum(6) +
+    criaBotãoNum(7) +
+    criaBotãoNum(8) +
+    criaBotãoNum(9) +
+    criaBotãoNum(0) +
+    criaBotãoOp ("+")
