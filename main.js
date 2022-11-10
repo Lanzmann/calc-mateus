a376cbd00b47b2ba87af562f788f79c8357fcb59
unknown <unidiogo@outlook.com>
2022-11-10 18:19:23 -0300

    Corrigida funcionalidade do botão apagar e do botão AC

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
-function adicionaNaTela(n) {
+var naTela = new Array();
 
-    document.querySelector('#tela-nums').innerHTML = n;
+function clicaBotão(n) {
+
+    if (n == "Ac")
+    {
+        naTela.length = 0;
+
+        atualizaTela();
+    }
+    else if (n == "bkspc")
+    {
+        console.log("oi");
+        naTela.pop();
+
+        atualizaTela();
+    }
+    else
+    {
+        naTela.push(n);
+
+        atualizaTela();
+    }
+  
 
 }
 
+function atualizaTela(){
+
+    document.querySelector('#tela-nums').innerHTML = naTela;
+
+    console.log("tela atualizada");
+
+    console.log(naTela)
+
+}
 
 function criaBotão(n) {
     
-    if (n == "<-")
+    if (typeof n === 'string')
     {
-        return `<div class="div<-"><button onclick="adicionaNaTela()"><img src="ico.svg">
-        </button></div>`
+        if (n == "bkspc")
+        {
+            return `<div class="div${n}"><button onclick="clicaBotão('${n}')"><img src="ico.svg"></button></div>`
+        }
+        
+        return `<div class="div${n}"><button onclick="clicaBotão('${n}')">${n}</button></div>`
     
     }
-    else{
-        return `<div class="div${n}"><button onclick="adicionaNaTela(${n})">${n}</button></div>`
+    else
+    {
+        return `<div class="div${n}"><button onclick="clicaBotão(${n})">${n}</button></div>`
     }
     
-    }
+}
 
 document.querySelector('#tela').innerHTML =
     '<div id="tela-nums">AB</div><div id="tela-op">CD</div>'
@@ -28,7 +63,7 @@ document.querySelector('#botoes-container').innerHTML =
     criaBotão("sin") +
     criaBotão("deg") +
     criaBotão("Ac") +
-    criaBotão("<-") +
+    criaBotão("bkspc") +
     criaBotão("/") +
     criaBotão("*") +
     criaBotão(7) +
