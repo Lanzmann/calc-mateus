4b25d4119c1767d7680345b0a28337b069b64ef6
unknown <unidiogo@outlook.com>
2022-11-19 08:31:23 -0300

    Finalmente adicionada função de operação simples entre 2 operandos!

--- main.js
+++ main.js
@@ -1,109 +1,59 @@
-var numerosNaMemoria = new Array();
-var operacoes = new Array();
-var botoesClicados = new Array();
-var numeral = new Array();
-var tela = new Array();
-
-
-
-// function constroiNum(n) {
-
-//     numeral.push(n);
-
-//     console.log("numeral é: " + numeral)
-//     console.log("agora n é: " + n)
-
-//     var numero = numeral.join('');
-
-//     console.log("join de:" + numero);
-
-//     return numero;
-
-// }
-
-
-function clicaBotão(n) {
-
-    console.log("botão clicado: " + n);
-
-    botoesClicados.push(n);
-
-    console.log("botoes clicados: " + botoesClicados);
-
-    if (typeof n === 'number') {
-
-        num = constroiNum(n);
-
-        console.log("colocando isso na tela: " + num)
-
-        numerosNaMemoria.push(num);
-
-        atualizaTelaNums();
-    }
-    else if (typeof n === 'string') {
-        if (n == "Ac") {
-            numerosNaMemoria.length = 0;
-            // botoesClicados.length = 0;
-            numeral.length = 0;
-
-            atualizaTelaNums();
-        }
-        else if (n == "bkspc") {
-
-            numerosNaMemoria.pop();
-            numeral.pop();
-
-            atualizaTelaNums();
-        }
-        else if (n == "+" || n == "-" || n == "*" || n == "/") {
-
-            numerosNaMemoria.push(num);
-
-            document.querySelector("#tela-nums2").innerHTML = op1;
-
-            atualizaTelaNums();
-
-        }
-    
-    }
- 
-}
-
-function atualizaTelaNums() {
-
-    /*tela = numerosNaMemoria.slice(-1) + */
-
-    document.querySelector('#tela-nums').innerHTML = numerosNaMemoria.slice(-1);
-
-}
-
 document.querySelector('#tela').innerHTML =
     '<div id="tela-nums"></div><div id="tela-op"></div>'
 
-resultado = $("#tela-op");
-var operando = false;
+var resultado = $("#tela-op");
+
+var estáOperando = false;
 var op = "";
 
 //mostra na tela valor do HTML do botão clicado
-
 jQuery(function(){$(".btn-num").on("click", function(){
     $("#tela-nums").append(($(this).html()))})});
 
+//botões de operadores guardam a operação e não permitem outra
 jQuery(function(){$(".btn-op").on("click", function(){
-    if (operando == false)
+    if (estáOperando == false)
     {
         op = $(this).html();
         $("#tela-nums").append(($(this).html()));
-        operando = true;
+        estáOperando = true;
     } 
 })});
 
+//botão igual separa os operandos na tela de nums e mostra o resultado na tela de resultados
 jQuery(function(){$(".btn-eq").on("click", function(){
         let operandos = $("#tela-nums").html().split(op);
         console.log(operandos);
+
+        function operacao(a,b)
+        {
+            if (op == "+")
+            {
+                return a+b
+            }
+
+            if (op == "-")
+            {
+                return a-b
+            }
+
+            if (op == "*")
+            {
+                return a*b
+            }
+
+            if (op == "/")
+            {
+                return a/b
+            }
+         
+        }
+
+        resultado.html(operacao(parseInt(operandos[0]), parseInt(operandos[1])));
+
 })});
 
-//cria botão com a regra diferente para operadores
+//cria botão com a regra diferente para operadores, bkspc e igual
 function criaBotão(n) {
       
         if (typeof n === "string")
